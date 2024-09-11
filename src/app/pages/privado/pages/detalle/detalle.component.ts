5180544203ac5396453f424a50f5a083428c7577
Alberto Marin <alberto.marind@gmail.com>
2024-09-11 12:04:06 -0600

    Se agregan estilos para la informacion de afiliacion en detalle

--- src/app/pages/privado/pages/detalle/detalle.component.ts
+++ src/app/pages/privado/pages/detalle/detalle.component.ts
@@ -62,6 +62,38 @@ export class DetalleComponent implements OnInit {
     },
   ];
 
+  afiliaciones: any[] = [
+    {
+      id: 1,
+      nombreRazonSocial: 'BBVA S.A. de C.V.',
+      clave: 'Registro Patronal',
+      valor: '2548769-10-1',
+      tipoRequerimiento: '-RFC-Domicilio-Actividad-Tipo de movimiento',
+    },
+  ];
+
+  historicos: any[] = [
+    {
+      id: 1,
+      registroPatronal: 'ITO101101HF2',
+      entidadFederativa: 'Baja California Sur',
+      subdelegacion: '33',
+      domicilio: 'Héroes de Chapultepec No. 655-B Lomas de la Soledad',
+    },
+  ];
+
+  historicosTerceros: any[] = [
+    {
+      id: 1,
+      nombrePatron: 'Formas e imágenes, S.A. de C.V.',
+      registroPatronal: 'ITO101101HF2',
+      entidadFederativa: 'Baja California Sur',
+      fechaAlta: '16/05/2021',
+      fechaBaja: '08/09/2023',
+      salarioBaseCotizacion: '$4,870.20',
+    },
+  ];
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
